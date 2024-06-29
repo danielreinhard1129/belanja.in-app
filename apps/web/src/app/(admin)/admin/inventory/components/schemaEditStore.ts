c8793e2df1f942d4156f4c953ec7cacf4ca9078a
MRTzee <teukumuksalmina280800@gmail.com>
2024-06-30 06:28:13 +0700

    fix: npm run build

--- apps/web/src/app/(admin)/admin/inventory/components/schemaEditStore.ts
+++ apps/web/src/app/(admin)/admin/inventory/components/schemaEditStore.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const schemaEditStore = z.object({
   name: z.string().min(4, "Name must be at least 4 characters long"),
-  city: z.string().min(4, "City must be at least 4 characters long"),
+  cityId: z.string().min(4, "City must be at least 4 characters long"),
   lat: z.string().min(4, "Lat must be at least 4 characters long"),
   long: z.string().min(4, "Long must be at least 4 characters long"),
   storeAdminId: z.string().min(1, "Choose a store"),
