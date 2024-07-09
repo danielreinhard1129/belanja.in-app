bf1849df33f05e0dfef727f7223a7a093e0a37d7
xpydrrloki <vvincent911@gmail.com>
2024-07-09 12:21:50 +0700

    fix: fix bug prisma transaction in create order

--- apps/web/src/hooks/api/transaction/useCreateNewOrder.tsx
+++ apps/web/src/hooks/api/transaction/useCreateNewOrder.tsx
@@ -4,12 +4,15 @@ import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import useAxios from "../useAxios";
 import { toast } from "sonner";
+import { useState } from "react";
 
 const useCreateNewOrder = () => {
   const { axiosInstance } = useAxios();
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const router = useRouter();
   const createNewOrder = async (payload: IOrderArgs) => {
     try {
+      setIsLoading(true)
       const location = localStorage.getItem("location");
       if (!location) {
         toast.error("Location access not given, not allowed to checkout")
@@ -24,13 +27,14 @@ const useCreateNewOrder = () => {
       router.push(`/order-details/${data.order.id}`)
     } catch (error) {
       if (error instanceof AxiosError) {
-        // toast.error({
-        //   description: error?.response?.data,
-        // });
+        toast.error(error.response?.data);
+        setIsLoading(false)
       }
+    } finally {
+      setIsLoading(false)
     }
   };
-  return { createNewOrder };
+  return { createNewOrder, isLoading };
 };
 
 export default useCreateNewOrder;
